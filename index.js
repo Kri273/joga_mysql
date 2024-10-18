8d1ffba5ddadbd5e203feb5571bbf378b2b33c30
Kristelle Tasane <kristelle.tasane@voco.ee>
2024-10-18 13:05:57 +0300

    Displayed all articles

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const path = require('path')
 const hbs = require('express-handlebars')
 // setup template endine directory and files extensions
 app.set('views', path.join(__dirname, 'views'));
-app.set('views engine', 'hbs')
-allCookies.engine('hbs', hbs.engine({
+app.set('view engine', 'hbs')
+app.engine('hbs', hbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
     layoutsDir: __dirname + '/views/layouts',
@@ -18,15 +18,16 @@ app.use(express.static('public'));
 
 const mysql = require('mysql2')
 
-const bodyParser = require('body-parser')
-app.use = (bodyParser.urlencoded({extended: true}))
+const bodyParser = require('body-parser');
+const { error } = require('console');
+app.use(bodyParser.urlencoded({extended: true}))
 
 // create database connection
 var con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'qwerty',
-    database: 'joga_mysql'
+    database: 'joga_mysql',
 })
 
 con.connect((err) => {
@@ -34,6 +35,19 @@ con.connect((err) => {
     console.log('connected to joga_mysql db')
 })
 
-app.listen(3006, () => {
-    console.log('App is started at http://localhost:3006')
+app.get('/', (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles =[]
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result
+        res.render('index',{
+            articles: articles
+        })
+    })
+    
+});
+   
+app.listen(3008, () => {
+    console.log('App is started at http://localhost:3008')
 })
